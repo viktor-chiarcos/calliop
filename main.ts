5eb0cb8e940c135cb0e5d0efb642e0044a510f31
VIKTOR CHIARCOS <viktor.chiarcos@gmail.com>
2025-06-30 08:07:39 +0200

    Neu: statt A+B jetzt P2

--- main.ts
+++ main.ts
@@ -1,23 +1,20 @@
-input.onPinTouchEvent(TouchPin.P1, input.buttonEventDown(), function on_pin_touch_p1() {
-    
+input.onPinTouchEvent(TouchPin.P1, input.buttonEventDown(), function () {
     Lautstärke = 103
 })
-input.onButtonEvent(Button.A, input.buttonEventClick(), function on_button_a() {
-    
+input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
     basic.showString("-")
     Lautstärke += -1
 })
-input.onButtonEvent(Button.AB, input.buttonEventClick(), function on_button_ab() {
+input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
+    basic.showString("+")
+    Lautstärke += 1
+})
+input.onPinTouchEvent(TouchPin.P2, input.buttonEventDown(), function () {
     music.setVolume(Lautstärke)
     music.play(music.stringPlayable("D G D A B C - G ", 120), music.PlaybackMode.UntilDone)
     music.play(music.stringPlayable("C E E C E E C C ", 120), music.PlaybackMode.UntilDone)
     music.playMelody("G G F F E E D - ", 120)
     music.playMelody("G G B G G B G G ", 120)
-})
-input.onButtonEvent(Button.B, input.buttonEventClick(), function on_button_b() {
-    
-    basic.showString("+")
-    Lautstärke += 1
 })
 let Lautstärke = 0
 let volume = Lautstärke
